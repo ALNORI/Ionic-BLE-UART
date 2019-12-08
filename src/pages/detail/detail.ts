fb2c5febb94feba941a554135f7a6bd032252e4f
Abdulbasit <abuamer04@hotmail.com>
2019-12-08 14:54:28 +0100

    added onwrite buffer

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -2,10 +2,21 @@ import { Component, NgZone } from '@angular/core';
 import { NavController, NavParams , AlertController } from 'ionic-angular';
 import { BLE } from '@ionic-native/ble';
 
-const BUTTON_SERVICE = 'a000';
+//const BUTTON_SERVICE = 'a000';
 //////const BUTTON_SERVICE = 'BLE UART-MINEW';
-const BUTTON_STATE_CHARACTERISTIC = 'a001';
+//const BUTTON_STATE_CHARACTERISTIC = 'a001';
 
+const LED_SERVICE = 'a000';
+const LED_CHARACERISTIC = 'a001';
+
+const UART_SERVICE = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
+const UART_TX_CHARACTERISTIC = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
+const UART_RX_CHARACTERISTIC = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
+
+
+const LIGHTBULB_SERVICE = 'ff10';
+const SWITCH_CHARACTERISTIC = 'ff11';
+const DIMMER_CHARACTERISTIC = 'ff12';
 
 @Component({
   selector: 'page-detail',
@@ -14,7 +25,8 @@ const BUTTON_STATE_CHARACTERISTIC = 'a001';
 export class DetailPage {
 
   peripheral: any = {};
-  buttonState: number;
+  power: boolean;
+  brightness: number;
   statusMessage: string;
 
   constructor(public navCtrl: NavController,
@@ -34,27 +46,55 @@ export class DetailPage {
 
   }
 
-  // the connection to the peripheral was successful
   onConnected(peripheral) {
 
     this.peripheral = peripheral;
     this.setStatus('Connected to ' + (peripheral.name || peripheral.id));
 
-    this.ble.startNotification(this.peripheral.id, BUTTON_SERVICE, BUTTON_STATE_CHARACTERISTIC).subscribe(
-      data => this.onButtonStateChange(data),
-      () => this.showAlert('Unexpected Error', 'Failed to subscribe for button state changes')
+    // Update the UI with the current state of the switch characteristic
+    this.ble.read(this.peripheral.id, LED_SERVICE, LED_CHARACERISTIC).then(
+      buffer => {
+        let data = new Uint8Array(buffer);
+        console.log('switch characteristic ' + data[0]);
+        this.ngZone.run(() => {
+            this.power = data[0] !== 0;
+        });
+      }
     )
+
+    // Update the UI with the current state of the dimmer characteristic
+     /* this.ble.read(this.peripheral.id, LIGHTBULB_SERVICE, DIMMER_CHARACTERISTIC).then(
+     buffer => {
+        let data = new Uint8Array(buffer);
+        console.log('dimmer characteristic ' + data[0]);
+        this.ngZone.run(() => {
+          this.brightness = data[0];
+        });
+      }
+    )*/
+  }
+
+  onPowerSwitchChange(event) {
+    console.log('onPowerSwitchChange');
+//*********************************** */
+    var data = new Uint8Array(1);
+
+//*********************************** */
+    data[0] = this.power ? 1 : 0;
+
+     this.ble.write(this.peripheral.id, LED_SERVICE , LED_CHARACERISTIC , data.buffer);
+      console.log("Sending: " + data[0]);
 
   }
 
-  onButtonStateChange(buffer:ArrayBuffer) {
-    var data = new Uint8Array(buffer);
-    console.log(data[0]);
+  setBrightness(event) {
 
-    this.ngZone.run(() => {
-      this.buttonState = data[0];
-    });
-
+       var data = new Uint8Array([this.brightness]);
+       console.log("Sending: " + data[0]);
+    this.ble.write(this.peripheral.id, LED_SERVICE, LED_CHARACERISTIC, data.buffer).then(
+      () => this.setStatus('Set brightness to ' + this.brightness),
+      e => this.showAlert('Unexpected Error', 'Error updating dimmer characteristic ' + e)
+    );
   }
 
   // Disconnect peripheral when leaving the page
